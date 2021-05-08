8defb3fea1bf2cfa07ed147681a66592a8a7c234
Ashishss21 <65999078+Ashishss21@users.noreply.github.com>
2021-05-08 20:42:01 +0530

    Deploy-Test-1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { ChatEngine } from 'react-chat-engine';
 import './App.css';
+import ChatFeed from './components/chatFeed';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () =>{
     return(
@@ -8,7 +10,7 @@ const App = () =>{
             projectID="0c5e9024-b273-4411-bc58-e10b4353bbe9"
             userName="ashishsengar2106"
             userSecret="Ash@2106"
-            renderChatFeed={Ash2106}
+            renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} /> }
         />
     );
 }
---
+++ src/components/Loader.js
@@ -0,0 +1,10 @@
+const Loader = () =>{
+    return(
+        <div class="d-flex justify-content-center" style={{marginTop:" 200px"}}>
+            <div class="spinner-border" role="status">
+                {/* <span class="sr-only">Loading...</span> */}
+            </div>
+        </div>
+    );
+}
+export default Loader;
