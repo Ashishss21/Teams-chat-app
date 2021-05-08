0466787b75dcbd0621c43cd640df07452be07e12
Ashishss21 <65999078+Ashishss21@users.noreply.github.com>
2021-05-08 08:50:39 +0530

    Build Commit

--- src/App.js
+++ src/App.js
@@ -1,2 +1,16 @@
-import {ChatEngine} from 'react-chat-engine';
-import 'App.css';
+import { ChatEngine } from 'react-chat-engine';
+import './App.css';
+
+const App = () =>{
+    return(
+        <ChatEngine
+            height="100vh"
+            projectID="0c5e9024-b273-4411-bc58-e10b4353bbe9"
+            userName="ashishsengar2106"
+            userSecret="Ash@2106"
+            renderChatFeed={}
+        />
+    );
+}
+
+export default App;
