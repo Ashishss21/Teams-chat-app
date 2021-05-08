cbc17eab7fa8eeac5f7addeb026dbee66e956e11
Ashishss21 <65999078+Ashishss21@users.noreply.github.com>
2021-05-08 08:57:45 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const App = () =>{
             projectID="0c5e9024-b273-4411-bc58-e10b4353bbe9"
             userName="ashishsengar2106"
             userSecret="Ash@2106"
-            renderChatFeed={}
+            renderChatFeed={Ash2106}
         />
     );
 }
